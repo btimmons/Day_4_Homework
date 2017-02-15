a3bbd0e24863531ee009b1a1e952b9e1e0f704fa
btimmons <brandis.timmons@gmail.com>
2017-02-15 17:10:07 -0500

    Day_4_Homework
    JS Exercises

---
+++ main2.js
@@ -0,0 +1,92 @@
+//Exercise 1
+
+var givenName;
+
+console.log(givenName);
+
+//What value is stored in 'givenName' right now?
+//undefined
+
+ var givenName = "Brett";
+ console.log(givenName);
+//What is 'givenName' set; to now?
+//Brett
+
+var givenName = givenName;
+console.log(givenName);
+//What is 'givenName' set to now?
+//Brett
+
+
+//Exercise 2
+ var givenName = "Brett";
+
+ var greeting = "Hello, how are you, ";
+
+ console.log(greeting + givenName + "?");
+
+ //Exercise 3
+
+ var high = 50;
+ var low =10;
+
+ var math = high - low;
+ console.log(math);
+ //what is math set to?
+ 40
+
+ math = high - "5";
+  console.log(math);
+ //what is math set to?
+ 45
+
+ math = "5" + "5" + 5
+console.log(math);
+
+ math = "a" + "b" + 5
+console.log(math);
+
+//Exercise 4
+
+var born = 1969;
+var today = 2017;
+var bday = today - born;
+
+
+console.log(bday);
+
+var yourName = "Brandi";
+var instructorName = "Joe";
+
+var statement = yourName + " is taking a class at the Iron Yard, and my instructor's name is " + instructorName + ".";
+console.log(statement);
+
+//Exercise 5
+
+var myAge = 45;
+
+if (myAge > 100) {
+  var isOld = true;
+  console.log(isOld);
+} else {
+  var isOld = false;
+  console.log(isOld);
+}
+
+//Exercise 6
+var firstName = "Julia";
+var lastName = "Roberts";
+var fullName = firstName + lastname; //This should be lastName
+
+console.log(fullName); // Julia Roberts
+
+var yearBorn = 1975;
+var currentYear = 2016;
+Var age = currentYear - yearBorn; //This should be var
+
+console.log(age); // 41
+
+
+
+
+///////
